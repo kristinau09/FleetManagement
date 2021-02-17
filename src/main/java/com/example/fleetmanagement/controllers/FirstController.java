ff7eb12cc3e394b89a236f896a53bc6befd2607e
kristinau09 <kristinau09@gmail.com>
2021-02-17 16:57:06 -0600

    Added static page with date and time
    Added two dependency for jsp and jstl

---
+++ src/main/java/com/example/fleetmanagement/controllers/FirstController.java
@@ -0,0 +1,20 @@
+package com.example.fleetmanagement.controllers;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class FirstController {
+	
+	@RequestMapping("/home.html")
+	public ModelAndView firstPage() {
+		
+		 Date dateAndTime = new Date();
+		
+		return new ModelAndView("/WEB-INF/jsp/welcome.jsp", "dateAndTime" , dateAndTime);
+	}
+
+}
