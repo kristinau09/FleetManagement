999886f2b514dce719ce559c899d88ef442d6759
kristinau09 <kristinau09@gmail.com>
2021-02-24 12:30:58 -0600

    Refactor and provided external service

--- src/main/java/com/example/fleetmanagement/FleetManagementApplication.java
+++ src/main/java/com/example/fleetmanagement/FleetManagementApplication.java
@@ -1,11 +1,13 @@
 package com.example.fleetmanagement;
 
 import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+
 @SpringBootApplication
-/* @EnableAutoConfiguration */
+
 public class FleetManagementApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/fleetmanagement/controllers/VehicleController.java
+++ src/main/java/com/example/fleetmanagement/controllers/VehicleController.java
@@ -17,6 +17,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.example.fleetmanagement.dao.VehicleRepository;
 import com.example.fleetmanagement.domain.Vehicle;
+import com.example.fleetmanagement.services.PositionTrackingExternalService;
 
 @Controller
 @RequestMapping("/website/vehicles")
@@ -25,13 +26,8 @@ public class VehicleController {
 	@Autowired
 	private VehicleRepository dao;	
 	
-	/*
-	 * this object will call eureka, and when it gets more than one instance back
-	 * from Eureka this object contains the intelligence to choose one of those
-	 * instances, not randomly, but using algorithm called round robin algorithm
-	 */
 	@Autowired
-	private LoadBalancerClient balancer;
+	private PositionTrackingExternalService externalService;
 	/* 
 	 * Presenting the initial form to the user
 	 */
@@ -82,31 +78,16 @@ public class VehicleController {
 	public ModelAndView showVehicleByName(@PathVariable("vehicleName") String vehicleName) {
 		
 		//search vehicle
-		Vehicle vName = dao.findByVehicleName(vehicleName);
+		Vehicle vName = dao.findByVehicleName(vehicleName);	
 		
-		//get the current position for this vehicle from the microservice
-		RestTemplate restTemplate = new RestTemplate();
+		PositionOfVehicle latestPosition = externalService.getLatestPositionForVehicleFromRemoteMicroservice(vehicleName);
 		
-		ServiceInstance service = balancer.choose("fleetman-position-tracker");
-		if(service == null) {
-			//service has crashed
-			throw new RuntimeException("Position tracker has crashed!");
-		}
-		
-		//call uri which returns the physical address where the service is located
-		String physicalLocation = service.getUri().toString();
-		
-		//making a rest request of current position of that vehicle
-		PositionOfVehicle response = restTemplate.getForObject(physicalLocation + "/vehicles/" + vehicleName, PositionOfVehicle.class);
-		
-		//get the port number where service in action for testing purpose
-		int port = service.getPort();
-		
+				
 		//put position and vehicle into the map
 		Map<String, Object> model = new HashMap<>();
 		model.put("vehicle", vName);
-		model.put("position", response);
-		model.put("port", port);
+		model.put("position", latestPosition);
+		
 		return new ModelAndView("vehicleInfo", "model", model);
 		
 		
